10b6292a237b99408fef518b4a2994863dee1784
cuongnk <cuongnk@thinhtientech.com>
2024-01-30 14:40:04 +0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,68 @@
 import { Component, OnInit } from '@angular/core';
 
+export const clientSettings = {
+  "data": {
+    "card_types": [
+      {
+        "code": "vn.national_id",
+        "name": "CMND cũ / CMND mới / CCCD / Hộ chiếu",
+        "orientation": "horizontal",
+        "has_back_side": true,
+        "front_qr": {
+          "exist": false
+        },
+        "back_qr": {
+          "exist": false
+        }
+      }
+    ],
+    "country": "vn",
+    "settings": {
+      "enable_compare_faces": true,
+      "enable_convert_pdf": true,
+      "enable_detect_id_card_tampering": true,
+      "enable_encryption": false,
+      "enable_face_retrieval": true,
+      "enable_index_faces": true,
+      "enable_read_id_card_info": true,
+      "enable_verify_face_liveness": true,
+      "enable_verify_id_card_sanity": true,
+      "enable_verify_portrait_sanity": true,
+      "liveness_modes": [
+        "active",
+        "passive"
+      ],
+      "scan_qr": "none",
+      "selfie_camera_options": [
+        "front"
+      ],
+      "selfie_enable_detect_multiple_face": true,
+      "support_transaction": false,
+      "utilities": {
+        "length_video_sec": 5,
+        "num_of_photo_taken": 3,
+        "photo_res": "640x640",
+        "timing_take_photo_sec": "1,2.5,4"
+      },
+      "web_app_crop_face": "none",
+      "web_ui": {
+        "index_collections": [
+          {
+            "id": "id_card",
+            "label": "Mặt trước CMND/CCCD/Passport"
+          },
+          {
+            "id": "portrait",
+            "label": "Hình ảnh selfie của khách hàng"
+          }
+        ],
+        "show_score": false
+      }
+    }
+  }
+}
+export const logCredentials = { enable: false }
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,24 +70,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   TVInstance: TVWebSDKInstance | null = null;
-
   livenessMode = 'active';
-
-  onChangeLivenessMode(event: Event) {
-    this.livenessMode = (event.target as HTMLSelectElement).value;
-  }
-
-  onStartReadIDCard() {
-    if (!this.TVInstance) return;
-    this.TVInstance.readIDCardUIOnly();
-  }
-
-  onLivenessDetection() {
-    if (!this.TVInstance) return;
-    this.TVInstance.livenessDetection({
-      mode: this.livenessMode,
-    });
-  }
 
   ngOnInit(): void {
     if (!window.TVWebSDK) return;
@@ -37,5 +83,25 @@ export class AppComponent implements OnInit {
 
     if (!this.TVInstance) return;
     this.TVInstance.runPreloadEKYCResources();
+  }
+
+  onStartReadIDCard() {
+    if (!this.TVInstance) return;
+    const params = {
+      clientSettings,
+      logCredentials
+    }
+    this.TVInstance.readIDCardUIOnly(params);
+  }
+
+  onChangeLivenessMode(event: Event) {
+    this.livenessMode = (event.target as HTMLSelectElement).value;
+  }
+
+  onLivenessDetection() {
+    if (!this.TVInstance) return;
+    this.TVInstance.livenessDetection({
+      mode: this.livenessMode,
+    });
   }
 }
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -4,7 +4,7 @@ declare global {
   }
 
   interface TVWebSDKInstance {
-    readIDCardUIOnly: () => void;
+    readIDCardUIOnly: (params: any) => void;
     livenessDetection: (options: { mode: string }) => void;
     runPreloadEKYCResources: () => void;
     destroyView: () => void;
