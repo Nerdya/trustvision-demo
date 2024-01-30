aa33d0deea4d78290ff2c3f995b7ac68a9b89aca
cuongnk <cuongnk@thinhtientech.com>
2024-01-30 11:57:36 +0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'trustvision-demo';
+export class AppComponent implements OnInit {
+  TVInstance: TVWebSDKInstance | null = null;
+
+  livenessMode = 'active';
+
+  onChangeLivenessMode(event: Event) {
+    this.livenessMode = (event.target as HTMLSelectElement).value;
+  }
+
+  onStartReadIDCard() {
+    if (!this.TVInstance) return;
+    this.TVInstance.readIDCardUIOnly();
+  }
+
+  onLivenessDetection() {
+    if (!this.TVInstance) return;
+    this.TVInstance.livenessDetection({
+      mode: this.livenessMode,
+    });
+  }
+
+  ngOnInit(): void {
+    if (!window.TVWebSDK) return;
+    this.TVInstance = new window.TVWebSDK.SDK({
+      container: document.getElementById('container'),
+      lang: 'vi',
+      assetRoot: 'https://unpkg.com/@tsocial/tvweb-sdk@5.17.0/assets',
+      enableAntiDebug: false,
+    });
+
+    if (!this.TVInstance) return;
+    this.TVInstance.runPreloadEKYCResources();
+  }
 }
